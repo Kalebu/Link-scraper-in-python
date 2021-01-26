34c99212af510924853326e92c773494993b7fad
Kalebu Gwalugano <isaackeinstein@gmail.com>
2021-01-26 10:59:50 +0300

    added src for link-scraper

---
+++ link_spider.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def extract_all_links(site):
+    html = requests.get(site).text
+    soup = BeautifulSoup(html, "html.parser").find_all("a")
+    links = [link.get("href") for link in soup]
+    return links
+
+
+if __name__ == "__main__":
+    site_link = input("Enter URL of the site : ")
+    all_links = extract_all_links(site_link)
+    print(all_links)
