b5d27b21c9a410120b712c933ca0045a5f721ff0
Mohammad Hossein Nouri <itismhn@yahoo.com>
2023-02-13 17:52:40 +0330

    error handling2

--- link_spider.py
+++ link_spider.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def extract_all_links(site):
     html = requests.get(site).text
     soup = BeautifulSoup(html, "html.parser").find_all("a")
@@ -9,10 +10,15 @@ def extract_all_links(site):
         print("=>",link," ")
     return ""
 
+
+def on_site_extraction_error(site_link):
+    print(f"Error: Could not extract links from {site_link}.")
+
+
 if __name__ == "__main__":
+    site_link = input("Enter URL of the site : ")
     try:
-        site_link = input("Enter URL of the site : ")
         all_links = extract_all_links(site_link)
         print(all_links)
-    except:
-            print("Enter Valid URL!")
+    except Exception as e:
+        on_site_extraction_error(site_link)
